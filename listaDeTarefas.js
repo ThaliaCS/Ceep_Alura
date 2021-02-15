737ede19050a81395cf17b3a2b8566065bf6b9c4
ThaliaCS <thalia.schone@gmail.com>
2021-02-15 10:08:49 -0300

    Done and delete button

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,16 +1,74 @@
-const newTask = document.querySelector('[data-form-button]');
+(() => {
+    const createTask = (evento) => {
 
-const createTask = (evento) => {
- 
-    evento.preventDefault();
-    const input = document.querySelector('[data-form-input]');
-   
+        evento.preventDefault();
 
-    const value = input.value;
+        const lista = document.querySelector('[data-list]');
+        const input = document.querySelector('[data-form-input]');
+        const value = input.value;
 
-    console.log(value);
+        const task = document.createElement('li');
 
-    input.value = " ";
-}
+        task.classList.add('task');
 
-newTask.addEventListener('click', createTask);
+        const conteudo = ` <p class="content">${value}</p>`;
+
+        task.innerHTML = conteudo;
+
+        task.appendChild(DoneButton())
+        task.appendChild(DeleteButton());
+        lista.appendChild(task);
+
+        input.value = " ";
+    }
+
+    const newTask = document.querySelector('[data-form-button]');
+
+
+    newTask.addEventListener('click', createTask);
+
+    const DoneButton = () => {
+
+        const doneButton = document.createElement('button');
+
+        doneButton.classList.add('check-button')
+        doneButton.innerText = 'concluir';
+        doneButton.addEventListener('click', completeTask);
+
+        return doneButton;
+    }
+
+    /* componente, começa com maiusculo */
+    const DeleteButton = () => {
+
+        const deleteButton = document.createElement('button');
+
+        deleteButton.classList.add('delete-button');
+        deleteButton.innerText = 'excluir';
+        deleteButton.addEventListener('click', deleteTask);
+
+        return deleteButton;
+    }
+
+    const deleteTask = (evento) => {
+
+        const deleteButton = evento.target;
+
+        const taskDone = deleteButton.parentElement;
+
+        taskDone.remove();
+    }
+
+
+    const completeTask = (evento) => {
+
+        const doneButton = evento.target;
+
+        const taskDone = doneButton.parentElement;
+
+        /* toggle retorna boolean  e risca  done no css*/
+        taskDone.classList.toggle('done');
+
+    }
+
+})()
